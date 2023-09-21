e92f811456b2301db7be55065fe9612629d0ab83
romain dufour <rdj.dufour@gmail.com>
2023-09-21 17:15:30 +0200

    repo 21-09-23 v5

--- typescript/exercices/exercices_typage/main.js
+++ typescript/exercices/exercices_typage/main.js
@@ -1,35 +1,27 @@
-//1
+"use strict";
 let userName = "romain";
-//2
 let userAge = 34;
-//3
 let isLogin = false;
-//4
 let userNames = [];
 userNames.push(userName);
 console.log(userNames);
-//5
 let person = {
     firstName: "Dupont",
     age: 20,
     isLoggedIn: true
 };
-// console.log(person.age); // non ce n'est pas possible d'afficher l'age
-//6
 let person1 = {
     firstName: "Dupont",
     age: 20,
     isLoggedIn: true
 };
 console.log(person1.age);
-//7
 let person2 = {
     firstName: "Dupont",
     age: 20,
     isLoggedIn: true
 };
 console.log(person2.age);
-//8
 let infos;
 infos = ["Dupont", 20];
 console.log(infos[1]);
