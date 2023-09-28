5b07aefe655ab305a4953e75a0b7fe1e34a85881
romain dufour <rdj.dufour@gmail.com>
2023-09-28 14:01:34 +0200

    repo 28-09-23

--- typescript/exercices/filter_recipes/assets/ts/main.js
+++ typescript/exercices/filter_recipes/assets/ts/main.js
@@ -1,4 +1,12 @@
 import { recipes } from "./data/recipes.js";
+const recipeName = document.querySelector("#recipeName");
+const prepTime = document.querySelector("#prepTime");
+const cookTime = document.querySelector("#cookTime");
+const recipeContainer = document.querySelector("#recipeContainer");
+const bntFilter = document.querySelector("#btnFilter");
+const btnRecipe = document.querySelector("#btnRecipe");
+const myModalContainer = document.querySelector("#myModalContainer");
+const editContactClose = document.querySelector("#editContactClose");
 const recipesList = [];
 for (const el in recipes) {
     recipesList.push(recipes[el]);
@@ -7,4 +15,39 @@ console.table(recipesList);
 // let recipeNamed:[] =recipesList[0] 
 // console.table(recipeName);
 console.log(recipesList[0].name);
-let recipeName = recipesList[0].name;
+// let affichageName: string = recipesList[0].name
+// let affichageCookingTime: string = recipesList[0].cookTime
+// let affichagePrepTime: string = recipesList[0].prepTime
+// console.log(affichageName);
+// console.log(affichageCookingTime);
+// console.log(affichagePrepTime);
+// recipeName.innerHTML = affichageName
+// prepTime.innerHTML = affichagePrepTime
+// cookTime.innerHTML = affichageCookingTime
+for (let i = 0; i < recipesList.length; i++) {
+    const newbouton = document.createElement("button");
+    newbouton.className = "btn btn-outline-light  w-100";
+    newbouton.innerHTML +=
+        `
+<div class="fw-bold fs-4" id="recipeName">${recipesList[i].name}</div>
+<hr>
+<div class="row">
+    <div class="col-6"><i class="bi bi-hourglass-split"></i>
+        <span id="prepTime">${recipesList[i].prepTime}</span></div>
+    <div class="col-6"><i class="bi bi-fire"></i>
+        <span id="cookTime">${recipesList[i].cookTime}</span></div>
+</div>
+`;
+    recipeContainer.appendChild(newbouton);
+    modal(newbouton);
+}
+function modal(arg) {
+    arg.addEventListener('click', (e) => {
+        myModalContainer.style.display = "block";
+        console.log(e);
+    });
+    editContactClose.addEventListener('click', (e) => {
+        myModalContainer.style.display = "none";
+        console.log(e);
+    });
+}
--- typescript/exercices/filter_recipes/assets/ts/main.ts
+++ typescript/exercices/filter_recipes/assets/ts/main.ts
@@ -1,25 +1,34 @@
-import {recipes } from "./data/recipes.js";
+import { recipes } from "./data/recipes.js";
 
+const recipeName = document.querySelector("#recipeName") as HTMLElement
+const prepTime = document.querySelector("#prepTime") as HTMLElement
+const cookTime = document.querySelector("#cookTime") as HTMLElement
+const recipeContainer = document.querySelector("#recipeContainer") as HTMLElement
+
+const bntFilter = document.querySelector("#btnFilter") as HTMLButtonElement
+const btnRecipe = document.querySelector("#btnRecipe") as HTMLButtonElement
+const myModalContainer = document.querySelector("#myModalContainer") as HTMLElement
+const editContactClose = document.querySelector("#editContactClose") as HTMLButtonElement
 
 
 interface Recipe {
-    id?: string;
+  id?: string;
+  name: string;
+  servings: number;
+  prepTime: string;
+  cookTime: string;
+  ingredients: {
     name: string;
-    servings: number;
-    prepTime: string;
-    cookTime: string;
-    ingredients: {
-      name: string;
-      amount: string;
-    }[];
-    instructions: string[];
-  }
+    amount: string;
+  }[];
+  instructions: string[];
+}
 
 const recipesList: Recipe[] = [];
 
 for (const el in recipes) {
 
-    recipesList.push(recipes[el])
+  recipesList.push(recipes[el])
 
 }
 console.table(recipesList);
@@ -28,4 +37,49 @@ console.table(recipesList);
 
 console.log(recipesList[0].name);
 
-let recipeName : string = recipesList[0].name
+// let affichageName: string = recipesList[0].name
+// let affichageCookingTime: string = recipesList[0].cookTime
+// let affichagePrepTime: string = recipesList[0].prepTime
+
+// console.log(affichageName);
+// console.log(affichageCookingTime);
+// console.log(affichagePrepTime);
+
+// recipeName.innerHTML = affichageName
+// prepTime.innerHTML = affichagePrepTime
+// cookTime.innerHTML = affichageCookingTime
+
+for (let i = 0; i < recipesList.length; i++) {
+
+  const newbouton = document.createElement("button")
+  newbouton.className = "btn btn-outline-light  w-100"
+  newbouton.innerHTML +=
+    `
+<div class="fw-bold fs-4" id="recipeName">${recipesList[i].name}</div>
+<hr>
+<div class="row">
+    <div class="col-6"><i class="bi bi-hourglass-split"></i>
+        <span id="prepTime">${recipesList[i].prepTime}</span></div>
+    <div class="col-6"><i class="bi bi-fire"></i>
+        <span id="cookTime">${recipesList[i].cookTime}</span></div>
+</div>
+`
+  recipeContainer.appendChild(newbouton)
+
+  modal(newbouton)
+}
+
+function modal(arg : HTMLElement) {
+  arg.addEventListener('click', (e) => {
+    myModalContainer.style.display = "block"
+    console.log(e);
+
+  })
+
+  editContactClose.addEventListener('click', (e) => {
+    myModalContainer.style.display = "none"
+    console.log(e);
+
+  })
+}
+
