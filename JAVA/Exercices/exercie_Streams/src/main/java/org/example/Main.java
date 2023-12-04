983428480d02810ea1c679624a0e76d7a0dbc1fc
romain dufour <rdj.dufour@gmail.com>
2023-12-04 17:19:49 +0100

    Java Objet streams - regex

--- JAVA/Exercices/exercie_Streams/src/main/java/org/example/Main.java
+++ JAVA/Exercices/exercie_Streams/src/main/java/org/example/Main.java
@@ -1,8 +1,9 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -11,26 +12,46 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
 
-     //   String texte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-        String texte = "La technologie moderne a radicalement transformé la façon dont nous interagissons avec le monde qui nous entoure. Des smartphones aux réseaux sociaux, en passant par l'intelligence artificielle, notre quotidien est profondément influencé par ces avancées. \n Les dispositifs intelligents connectés nous offrent une accessibilité sans précédent à l'information, transformant la manière dont nous communiquons, apprenons et travaillons. Cependant, cette omniprésence technologique suscite également des questions éthiques et sociales. Les préoccupations liées à la confidentialité des données, à la dépendance aux écrans et à l'impact sur l'emploi sont autant de défis que la société doit relever. L'équilibre entre progrès technologique et préservation des valeurs humaines devient ainsi une préoccupation majeure.\n Dans le domaine de la santé, la technologie a permis des avancées significatives, de la médecine personnalisée aux applications de suivi de la condition physique. Cependant, les discussions sur l'éthique de l'utilisation des données médicales et la sécurité des dispositifs médicaux connectés sont au cœur des débats. \n Malgré ces défis, l'innovation technologique reste un moteur de progrès. Des solutions émergent pour relever les défis actuels, allant de la régulation des technologies émergentes à la promotion de l'éducation numérique. En fin de compte, l'avenir de la technologie dépend de la manière dont nous intégrons ces avancées dans notre vie quotidienne tout en préservant notre humanité.";
 
-        List <String> mots = Arrays.asList(texte.replaceAll(",","").replaceAll("\\.","").toLowerCase().split(" "));
+        // Lire a partir d'un fichier texte
+        try {
+            Scanner scanner = new Scanner(new File("path/to/file"));
+            StringBuilder textBuilder = new StringBuilder();
+            do {
+                textBuilder.append(scanner.nextLine());
+            } while (scanner.hasNextLine());
+            String allText = textBuilder.toString();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        }
+        
+
+                //   String texte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+                String
+        texte = "La technologie moderne a radicalement transformé la façon dont nous interagissons avec le monde qui nous entoure. Des smartphones aux réseaux sociaux, en passant par l'intelligence artificielle, notre quotidien est profondément influencé par ces avancées. \n Les dispositifs intelligents connectés nous offrent une accessibilité sans précédent à l'information, transformant la manière dont nous communiquons, apprenons et travaillons. Cependant, cette omniprésence technologique suscite également des questions éthiques et sociales. Les préoccupations liées à la confidentialité des données, à la dépendance aux écrans et à l'impact sur l'emploi sont autant de défis que la société doit relever. L'équilibre entre progrès technologique et préservation des valeurs humaines devient ainsi une préoccupation majeure.\n Dans le domaine de la santé, la technologie a permis des avancées significatives, de la médecine personnalisée aux applications de suivi de la condition physique. Cependant, les discussions sur l'éthique de l'utilisation des données médicales et la sécurité des dispositifs médicaux connectés sont au cœur des débats. \n Malgré ces défis, l'innovation technologique reste un moteur de progrès. Des solutions émergent pour relever les défis actuels, allant de la régulation des technologies émergentes à la promotion de l'éducation numérique. En fin de compte, l'avenir de la technologie dépend de la manière dont nous intégrons ces avancées dans notre vie quotidienne tout en préservant notre humanité.";
+
+        List<String> mots = Arrays.asList(texte.replaceAll(",", "").replaceAll("\\.", "").toLowerCase().split(" "));
 
         //fréquence des mots
-        System.out.println(mots.stream().collect((Collectors.groupingBy(Function.identity(),Collectors.counting()))));
+        System.out.println(mots.stream().collect((Collectors.groupingBy(Function.identity(), Collectors.counting()))));
         System.out.println("**************");
 
         // filtrer les mots
         int lengh = 5;
-        System.out.println(mots.stream().filter(m -> m.length() < lengh ).collect(Collectors.toList()));
+        System.out.println(mots.stream().filter(m -> m.length() < lengh).collect(Collectors.toList()));
         System.out.println("**************");
 
         // Mots uniques
-        System.out.println(mots.stream().distinct().collect(Collectors.toList()));
+        System.out.println(mots.stream().distinct().collect(Collectors.toSet()));
         System.out.println("**************");
+        Set<String> liste = new HashSet<>(mots);
+        System.out.println("liste : " + liste);
+
 
         //Mots les plus fréquents
-      //  System.out.println(mots.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()).entrySet().stream));
+        System.out.println("**************");
+
+        //  System.out.println(mots.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()).entrySet().stream));
 
         Map<String, Long> frequenceDesMots = mots.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
@@ -49,7 +70,7 @@ public class Main {
 
         System.out.println("**************");
 
-        System.out.println("mot le plus court : " + mots.stream().min((mot1, mot2) -> Integer.compare(mot1.length(), mot2.length())).orElse("") );
+        System.out.println("mot le plus court : " + mots.stream().min((mot1, mot2) -> Integer.compare(mot1.length(), mot2.length())).orElse(""));
 
         System.out.println("**************");
 
@@ -58,5 +79,14 @@ public class Main {
         System.out.println("**************");
 
 
+    }
+
+    public static void readFromFile() {
+
+        try (Scanner scanner = new Scanner(new File("path/to/file"))) {
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
