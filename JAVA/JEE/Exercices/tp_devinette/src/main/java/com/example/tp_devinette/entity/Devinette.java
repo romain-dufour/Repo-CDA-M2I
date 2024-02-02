c925281509c2961cb7f8b4bb2af18dd9c28e50d1
romain <rdj.dufour@gmail.com>
2024-02-02 09:51:08 +0100

    debut tp devinette

---
+++ JAVA/JEE/Exercices/tp_devinette/src/main/java/com/example/tp_devinette/entity/Devinette.java
@@ -0,0 +1,7 @@
+package com.example.tp_devinette.entity;
+import jakarta.persistence.Entity;
+
+@En
+public class Devinette {
+
+}
---
+++ JAVA/JEE/Exercices/tp_devinette/src/tp_devinette.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS tp_devinette;
+USE tp_devinette;
+DROP DATABASE tp_devinette;
