f3da03af995ca560f9d621eaaa9228aee8e9a6b8
romain <rdj.dufour@gmail.com>
2024-02-16 09:14:21 +0100

    debut tp spring blog

---
+++ Spring/Exercices/TP_vendredi_blog/src/main/java/org/example/tp_vendredi_blog/TpVendrediBlogApplication.java
@@ -0,0 +1,13 @@
+package org.example.tp_vendredi_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpVendrediBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpVendrediBlogApplication.class, args);
+    }
+
+}
